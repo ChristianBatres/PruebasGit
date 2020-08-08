2311a958edda73ffab191b913a2aee12aac13ca8
batres <cbatresaleman97@gmail.com>
2020-08-07 20:20:24 -0600

    prueba

---
+++ primerversion.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+     <link href="css/icons.css" rel="stylesheet">
+        <title>GIT</title>
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/icon.css"  media="screen,projection"/>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <!--  archivos css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/mystyle-sheet.css" media="screen,projection"/>
+  
+</head>
+<body>
+
+<div class='container'>
+        <div class="card-panel">
+            <div id="titulo" class="center-heading">
+                <h3>GIT</h3>
+                <span class="center-line blue"></span>
+            </div>
+           <br>
+            <p>“Git fue creado pensando en la eficiencia y la confiabilidad del mantenimiento de
+            versiones de aplicaciones cuando éstas tienen un gran número de archivos de código
+            fuente, es decir Git nos proporciona las herramientas para desarrollar un trabajo en
+            equipo de manera inteligente y rápida y por trabajo nos referimos a algún software
+            o página que implique código el cual necesitemos hacerlo con un grupo de
+            personas”
+            </p>
+            
+            <br>
+            <p>
+                “Los Sistemas de Control de Versiones Distribuidos (DVCS) salvan este problema.
+                Algunos ejemplos de sistemas distribuidos, aparte de Git, son Mercurial, Bazaar o
+                Darcs. En este tipo de herramientas, los clientes replican completamente el
+                repositorio.”
+            </p>
+            
+        </div>
+    </div>
+
+    <!-- JavaScripts -->
+<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+<!-- Importar materialiaze.js -->
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript" src="js/myjs.js"></script>
+</body>
+</html>
